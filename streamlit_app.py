05ad22763c39c63091db7433ae0c2013af8e5da6
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-02-12 15:54:22 -0500

    updating req

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,10 @@ import pandas as pd
 from snowflake.snowpark import Session
 
 from pages import intro
-from pages import analytics
-from pages import engineering
-from pages import enablement
-from pages import apps
-from pages import architecture
-from pages import managed_services
-from pages import tech_ops
-from pages import ai_ml
-from pages import em
+
 
 PAGES = {
-        "Intro": intro,
-        "Analytics": analytics,
-        "Engineering": engineering,
-        "Enablement": enablement,
-        "Apps": apps,
-        "Architecture": architecture,
-        "Managed Services": managed_services,
-        "TechOps": tech_ops,
-        "AI/ML": ai_ml,
-        "EM": em,
+        "Intro": intro
     }
 
 def main():
