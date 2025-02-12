f900ebee456e65ecc5fff2c8aea002d1992a8cf1
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-02-12 16:00:27 -0500

    reset intro page

--- pages/intro.py
+++ pages/intro.py
@@ -31,18 +31,6 @@ def create_snowflake_session(snowflake_account, snowflake_user, snowflake_passwo
   snowflake_session = Session.builder.configs(options).create()
   return snowflake_session
 
-@st.cache(allow_output_mutation=True)
-def get_snowflake_session():
-    snowflake_account = st.session_state.get("snowflake_account")
-    snowflake_user = st.session_state.get("snowflake_user")
-    snowflake_db = st.session_state.get("snowflake_db")
-    snowflake_schema = st.session_state.get("snowflake_schema")
-    snowflake_password = st.session_state.get("snowflake_password")
-    snowflake_role = st.session_state.get("snowflake_role")
-    snowflake_wh = st.session_state.get("snowflake_wh")
-    return create_snowflake_session(snowflake_account, snowflake_user, snowflake_password, snowflake_role, snowflake_wh, snowflake_db, snowflake_schema)
-    
-
 def workbook_types():
     faq_markdown = """  
         ## 1. Overview
