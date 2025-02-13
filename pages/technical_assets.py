471d8d81cbb35d974bbdda9b187a89ae3b551589
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-02-13 14:05:43 -0500

    removing columns to test dependencies

--- pages/technical_assets.py
+++ pages/technical_assets.py
@@ -24,12 +24,9 @@ def main():
             st.dataframe(test_df, hide_index=True)    
             st.write("")
             description()
-            col1, col2 = st.columns([1, 3])
-            with col1:
-                select_filter = st.radio("Select One", ["Primary Domain", "Use Case"])
-            with col2:
-                if select_filter == "Primary Domain":
-                    selected_domain = st.radio("Select One", ["Asset management", "Finance", "Procurment", "Compliance", "Operations"],
+            select_filter = st.radio("Select One", ["Primary Domain", "Use Case"])
+            if select_filter == "Primary Domain":
+                selected_domain = st.radio("Select One", ["Asset management", "Finance", "Procurment", "Compliance", "Operations"],
                              captions=[
                                  "Related Domain(s): Operations",
                                  "Related Domain(s): Asset management",
@@ -37,13 +34,13 @@ def main():
                                  "Related Domain(s): Asset management, Operations",
                                  "Related Domain(s): Asset management, Finance",
                              ],)
-                    if selected_domain == "Asset management":
-                        test_query = "SELECT * from USE_CASE_INVENTORY"
+                if selected_domain == "Asset management":
+                    test_query = "SELECT * from USE_CASE_INVENTORY"
                     
-                    else:
-                        test_query = "SELECT * FROM USE_CASE_INVENTORY WHERE asset_id = 1"
-                if select_filter == "Use Case":
-                    selected_use_case = st.radio("Select One", ["Asset Lifecycle Optimization", "Depreciation Forecasting Accuracy", "Vendor Performance Management", "Regulatory Compliance Automation", "Predictive Maintenance for Assets"],
+                else:
+                    test_query = "SELECT * FROM USE_CASE_INVENTORY WHERE asset_id = 1"
+            if select_filter == "Use Case":
+                selected_use_case = st.radio("Select One", ["Asset Lifecycle Optimization", "Depreciation Forecasting Accuracy", "Vendor Performance Management", "Regulatory Compliance Automation", "Predictive Maintenance for Assets"],
                              captions=[
                                  "Business Use Case: Implement predictive maintenance and usage-based lifecycle tracking.",
                                  "Business Use Case: Enhance depreciation models using real-time asset usage and condition data.",
@@ -51,11 +48,11 @@ def main():
                                  "Business Use Case: Build a compliance dashboard that monitors asset-related regulations in real time.",
                                  "Business Use Case: Implement IoT-based sensors and machine learning models for failure prediction.",
                              ],)
-                    if selected_use_case == "Asset Lifecycle Optimization":
-                        test_query = "SELECT * from USE_CASE_INVENTORY"
+                if selected_use_case == "Asset Lifecycle Optimization":
+                    test_query = "SELECT * from USE_CASE_INVENTORY"
                     
-                    else:
-                        test_query = "SELECT * FROM USE_CASE_INVENTORY WHERE asset_id = 1"
+                else:
+                    test_query = "SELECT * FROM USE_CASE_INVENTORY WHERE asset_id = 1"
             
             test_df = session.sql(test_query).to_pandas()
             st.dataframe(test_df,hide_index=True)
