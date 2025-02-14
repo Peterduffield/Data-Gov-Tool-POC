e3bebb50ad2562e8f8455363095237550f9b91e2
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-02-13 19:11:03 -0500

    UDATING EMPLOYEE PAGE

--- pages/employees.py
+++ pages/employees.py
@@ -16,45 +16,26 @@ def main():
     if session:    
         try:
             # Execute SQL queries
-            test_query = "SELECT * FROM USE_CASE_INVENTORY"
-            test_df = session.sql(test_query).to_pandas()
-            test_query_2 = "SELECT * FROM USE_CASE_INVENTORY WHERE asset_id = 1"
-            test_df_2 = session.sql(test_query_2).to_pandas()
-            
+            select_all_employee_query = "SELECT * FROM EMPLOYEES"
+
+           
+            select_all_employee_df = session.sql(select_all_employee_query).to_pandas()
+            employee_name_list = select_all_employee_df['EMPLOYEE_NAME'].tolist()
 
             # Display data            
-            st.title("Use Case and Domain Inventory") 
+            st.title("Employee Catalog") 
             st.write("")
             description()
-            select_filter = st.radio("Select One", ["Primary Domain", "Use Case"])
-            if select_filter == "Primary Domain":
-                selected_domain = st.radio("Select One", ["Asset management", "Finance", "Procurment", "Compliance", "Operations"],
-                             captions=[
-                                 "Related Domain(s): Operations",
-                                 "Related Domain(s): Asset management",
-                                 "Related Domain(s): Operations, Finance",
-                                 "Related Domain(s): Asset management, Operations",
-                                 "Related Domain(s): Asset management, Finance",
-                             ],)
-                if selected_domain == "Asset management":
-                    st.dataframe(test_df,hide_index=True)
-                else:
-                    st.dataframe(test_df_2,hide_index=True)
-            if select_filter == "Use Case":
-                selected_use_case = st.radio("Select One", ["Asset Lifecycle Optimization", "Depreciation Forecasting Accuracy", "Vendor Performance Management", "Regulatory Compliance Automation", "Predictive Maintenance for Assets"],
-                             captions=[
-                                 "Business Use Case: Implement predictive maintenance and usage-based lifecycle tracking.",
-                                 "Business Use Case: Enhance depreciation models using real-time asset usage and condition data.",
-                                 "Business Use Case: Develop a vendor scorecard tracking repair quality, cost, and response time.",
-                                 "Business Use Case: Build a compliance dashboard that monitors asset-related regulations in real time.",
-                                 "Business Use Case: Implement IoT-based sensors and machine learning models for failure prediction.",
-                             ],)
-                if selected_use_case == "Asset Lifecycle Optimization":
-                    st.dataframe(test_df,hide_index=True)
-                else:
-                    st.dataframe(test_df_2,hide_index=True)
- 
-                
+            selected_employee = st.selectbox("Select One", employee_name_list)
+            col1, col2 = st.columns(2)
+            with col1:
+                if selected_employee != "Select One":
+                    employee_business_role = select_all_employee_df[select_all_employee_df["EMPLOYEE_NAME"] == selected_employee]
+                    st.markdown(f"<h1> {employee_business_role.shape[3]} </h1>", unsafe_allow_html=True) 
+            with col2:
+                if selected_employee != "Select One":
+                    employee_business_role = select_all_employee_df[select_all_employee_df["EMPLOYEE_NAME"] == selected_employee]
+                    st.markdown(f"<h1> {employee_business_role.shape[4]} </h1>", unsafe_allow_html=True)                 
         except Exception as e:
             st.error(f"Error fetching data from Snowflake: {str(e)}")
 if __name__ == "__main__":
