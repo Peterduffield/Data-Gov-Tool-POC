f79ef4e9f3e672ebf693d0f812c40106253191cc
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-19 17:43:45 -0400

    moving logo to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,20 @@ business_glossary_tbl = session.sql("SELECT * FROM BUSINESS_GLOSSARY").to_pandas
 key_term_list = business_glossary_tbl['KEY_BUSINESS_TERM_NAME'].to_list()
 
 def main():
-    col1, col2 = st.columns([1,3], gap="large")
+    col1, col2, col3 = st.columns([1,3,1], gap="large")
     with col1:
-        st.image("https://tercera.io/wp-content/uploads/2021/11/hakkoda_logo.png")
+        st.write(" ")
     with col2:
         st.title("Data Governance Tool")
-
-    col3, col4, col5 = st.columns([2,3,1])
     with col3:
-        st.write(" ")    
+        st.write(" ")
+
+    col4, col5, col6 = st.columns([1,3,1], gap="large")
     with col4:
-        st.subheader("Key Business Term Glossary")
+        st.write(" ")    
     with col5:
+        st.subheader("Key Business Term Glossary")
+    with col6:
         st.write(" ") 
    
     selected_business_term = st.selectbox("Select a Business Term", key_term_list,index=None)
@@ -41,6 +43,6 @@ def main():
     
     # Display results in Streamlit
     st.dataframe(business_glossary_tbl)
-
+    st.image("https://tercera.io/wp-content/uploads/2021/11/hakkoda_logo.png")
 if __name__ == "__main__":
     main()
