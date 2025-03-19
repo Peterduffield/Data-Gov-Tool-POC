495b2bf6c6cd949cf4b7a1ed2372b571a108bbf8
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-19 17:40:44 -0400

    spacing test

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ business_glossary_tbl = session.sql("SELECT * FROM BUSINESS_GLOSSARY").to_pandas
 key_term_list = business_glossary_tbl['KEY_BUSINESS_TERM_NAME'].to_list()
 
 def main():
-    col1, col2 = st.columns([1,4], gap="medium")
+    col1, col2 = st.columns([1,3], gap="large")
     with col1:
         st.image("https://tercera.io/wp-content/uploads/2021/11/hakkoda_logo.png")
     with col2:
