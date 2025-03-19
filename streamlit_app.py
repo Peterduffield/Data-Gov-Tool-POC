13b4c633a9a3593788fb7a59eb3e1e130fb42f31
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-19 15:42:03 -0400

    refresh and restart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,11 @@
-from mmap import PAGESIZE
 from snowflake.snowpark import Session
-import streamlit as st
-from pages import intro
-from pages import technical_assets
-from pages import employees
-
-
-
 import streamlit as st
 
 def main():
-    st.sidebar.title("Navigation")
 
-    # Define PAGES before using it
-    PAGES = {
-        "INTRO": intro,
-        "Domain Inventory": technical_assets,
-        "Assets by Employee": employees,
-    }
-
-    if st.session_state.get('session', False): 
-        choice = st.sidebar.radio("Select a page:", list(PAGES.keys()))
-        if choice in PAGES:
-            PAGES[choice].main()
-
+    conn = st.connection("snowflake")
+    df = conn.query("SELECT * FROM BUSINESS_GLOSSARY")
+    table = df.to_pandas()
+    st.dataframe(table)
 if __name__ == "__main__":
     main()
