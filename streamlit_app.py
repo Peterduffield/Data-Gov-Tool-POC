1cd0f8e7cae167a236c9aee4d3c25ea9a4456e96
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-20 17:09:17 -0400

    testing iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,6 +83,7 @@ def main():
         st.write('Related Critical Data Element(s)')
 
     related_catalog_id_df = data_catalog_tbl[data_catalog_tbl['CATALOG_ID'].isin(filtered_df['RELATED_TO_CATALOG_ID_S_'])]
+    st.write(data_catalog_tbl["CATALOG_ID"].iloc[0])
     st.dataframe(related_catalog_id_df)
     st.dataframe(data_catalog_tbl)
 
