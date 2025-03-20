a57dd41f1d77294de08337d26879cd04cdc4d128
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-20 17:27:15 -0400

    removing to test error

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,8 +83,8 @@ def main():
         st.write('Related Critical Data Element(s)')
 
         related_catalog_id_df = data_catalog_tbl[data_catalog_tbl['CATALOG_ID'].isin(filtered_df['RELATED_TO_CATALOG_ID_S_'])]
-        related_attribute_name = related_catalog_id_df['ATTRIBUTE_NAME'].iloc[0]
-        st.markdown(f" ## {related_attribute_name}", unsafe_allow_html=True)
+        #related_attribute_name = related_catalog_id_df['ATTRIBUTE_NAME'].iloc[0]
+        #st.markdown(f" ## {related_attribute_name}", unsafe_allow_html=True)
 
     
     st.dataframe(related_catalog_id_df)
