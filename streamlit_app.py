9271beb5616b322aaa09e137c9124503199bf222
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-20 17:14:19 -0400

    testing iloc in markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,15 +81,8 @@ def main():
     
     with col5:
         st.write('Related Critical Data Element(s)')
-
-
-    data_catalog_tbl["CATALOG_ID"] = data_catalog_tbl["CATALOG_ID"].astype(str)
-    print(data_catalog_tbl["CATALOG_ID"].dtypes)
-
-    
+        st.markdown(f" ## {related_catalog_id_df["ATTRIBUTE_NAME"].iloc[0]}", unsafe_allow_html=True)
     related_catalog_id_df = data_catalog_tbl[data_catalog_tbl['CATALOG_ID'].isin(filtered_df['RELATED_TO_CATALOG_ID_S_'])]
-    st.write(data_catalog_tbl["CATALOG_ID"].iloc[0])
-    st.write(filtered_df['RELATED_TO_CATALOG_ID_S_'].iloc[0])
     st.dataframe(related_catalog_id_df)
     st.dataframe(data_catalog_tbl)
 
