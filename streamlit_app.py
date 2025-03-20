cd8d21cccf10d14a78c089ce648f5a9beba31605
Peter_Duffield_HKKD <157161814+Peterduffield@users.noreply.github.com>
2025-03-20 17:43:59 -0400

    testing related string mkdwon

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,6 +96,11 @@ def main():
         st.markdown(f" ## {related_attribute_name}, {related_attribute_db}", unsafe_allow_html=True)
     
 
+    # Combine attributes and databases into a formatted string
+    related_info = ", ".join(
+        [f"Attribute: {attr}  Database: {db}" for attr, db in zip(related_attribute_name, related_attribute_db)]
+    )
+    st.markdown(f"## {related_info}", unsafe_allow_html=True)
 
     st.write(data_catalog_tbl["CATALOG_ID"].iloc[0])
     st.write(filtered_df['RELATED_TO_CATALOG_ID_S_'].iloc[0])
